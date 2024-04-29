d5b8820838e5e790bd31d061377e27f8a0d025ba
AllenKashiwa <allenkashiwa@gmail.com>
2024-04-30 00:56:45 +0800

    add ps dot

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme Paradox
+function vim {nvim}
+function gs {git status}
+function ga {git add .}
+function gm {git commit -m}
+function cdd {cd ~}
